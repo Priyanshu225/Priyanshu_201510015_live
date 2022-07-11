53126b526a61174e01d3627f44064359a8e94fd8
Priyanshu225 <priyanshugupta2252002@gmail.com>
2022-07-11 21:53:08 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var n=document.createElement("span");
+n.innerHTML="Priyanshu Gupta"+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0";
+document.getElementById("js").appendChild(n);
+
+var roll=document.createElement("span");
+roll.innerHTML="201510015"+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0";
+document.getElementById("js").appendChild(roll);
+
+var coll=document.createElement("span");
+coll.innerHTML="GLA University"+"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0";
+document.getElementById("js").appendChild(coll);
+
+var date=document.createElement("span");
+date.innerHTML="11-07-2022";
+document.getElementById("js").appendChild(date);
